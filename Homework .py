1b79566a8f775f6dad6a1a3ad26b3677a61d1817
Kirill <kiril-karp4@mail.ru>
2022-10-17 19:44:53 +1000

    Task4

--- Homework .py
+++ Homework .py
@@ -64,3 +64,64 @@ koordinate = inputKoord(2)
 checkCoordinates(koordinate)
 
 """
+
+
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+"""""
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
+    """
+
+
+   # Напишите программу, которая принимает на вход координаты двух точек 
+    # и находит расстояние между ними в 2D пространстве. 
+      # Пример: - A (3,6); B (2,1) -> 5,09 - A (7,-5); B (1,-1) -> 7,21
+
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Ты ошибся. Вводить надо целые числа!")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
